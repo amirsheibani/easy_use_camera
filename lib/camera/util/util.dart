3758f92cb6a2d10cfa24755a0902f3cac2149a9c
a_sheibani <a_sheibani@keywa.ca>
2023-11-29 16:33:56 +0330

    add file or Uint8List to convert type

--- lib/camera/util/util.dart
+++ lib/camera/util/util.dart
@@ -1,26 +1,39 @@
 import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:image/image.dart';
 
-enum ConvertType{
-  jpg,png,
+enum ConvertType {
+  jpg,
+  png,
 }
-Future<File> cropImageAndConvert({
-  required File file,
-  required double aspectRatio,
-  ConvertType? convertType = ConvertType.jpg,
-}) async {
-  final bytes = file.readAsBytesSync();
-  Image? image = decodeImage(bytes);
-  if (image == null) return file;
+
+Future<File?> cropImageAndConvert({File? file, Uint8List? bytes, required double aspectRatio, ConvertType? convertType = ConvertType.jpg}) async {
+  Uint8List? _bytes;
+  if (file != null) {
+    _bytes = file.readAsBytesSync();
+  } else if (bytes != null) {
+    _bytes = bytes;
+  }
+  Image? image = decodeImage(_bytes!);
+  if (image == null) return file ?? File.fromRawPath(bytes!);
+
   const extraWidth = 26;
   final width = image.width - 26;
   final height = width / aspectRatio;
   final x = (extraWidth / 2).floor();
   final y = ((image.height / 2) - height / 2).floor();
-  var thumbnail = copyCrop(image, x: x,y: y,width: width.ceil(),height: height.ceil(),);
-  if(convertType == ConvertType.jpg){
-    return file.writeAsBytes(encodeJpg(thumbnail));
-  }else{
-    return file.writeAsBytes(encodePng(thumbnail));
+  var thumbnail = copyCrop(image, x: x, y: y, width: width.ceil(), height: height.ceil());
+  if (file != null) {
+    if (convertType == ConvertType.jpg) {
+      return file.writeAsBytes(encodeJpg(thumbnail));
+    } else {
+      return file.writeAsBytes(encodePng(thumbnail));
+    }
+  } else {
+    if (convertType == ConvertType.jpg) {
+      return File.fromRawPath(encodeJpg(thumbnail));
+    } else {
+      return File.fromRawPath(encodePng(thumbnail));
+    }
   }
 }
