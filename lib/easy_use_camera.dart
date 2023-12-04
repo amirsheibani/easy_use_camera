b47162818a7988075696f283b3e39bc919c65fd6
a_sheibani <a_sheibani@keywa.ca>
2023-12-04 13:56:01 +0330

    add image/video preview and some tools

--- lib/easy_use_camera.dart
+++ lib/easy_use_camera.dart
@@ -7,5 +7,6 @@ export 'package:easy_use_camera/camera/widget/scan_face.dart';
 export "package:camera_platform_interface/camera_platform_interface.dart" show XFile;
 export 'package:easy_use_camera/camera/dialog/camera_dialog.dart';
 export 'package:easy_use_camera/camera/preview_video.dart';
+export 'package:easy_use_camera/camera/preview_image.dart';
 
 
